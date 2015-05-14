ce20d9a08c214c96c6588596bd963dfe512d3705
Zenidal <1ochka1994@mail.ru>
2015-05-14 18:37:54 +0300

    Something added

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 var user = require('./user');
-var config = required('./configuration');
+var config = require('./configuration');
 
 
 function setResHeader(req, res, next) {
